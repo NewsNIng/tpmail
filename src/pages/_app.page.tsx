e17270f0d3556814be31a82534b165e77b3a103f
NewsNIng <iammsr7666@gmail.com>
2023-08-20 13:08:42 +0800

    feat: add umami

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -5,15 +5,23 @@ import { type AppType } from 'next/app'
 import { api } from '@/utils/api'
 
 import '@/styles/globals.css'
+import Script from 'next/script'
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+      <Script
+        async
+        src='https://analytics.umami.is/script.js'
+        data-website-id='8833a982-0876-4676-9f77-79603384d445'
+      />
+    </>
   )
 }
 
